93fb40be0095877fc5876baab3d258b969517e2e
Kapitan Rebrow <36919372+DominikFerenc@users.noreply.github.com>
2020-10-09 23:18:21 +0200

    Update scanner.py
    improved socket initiation

--- scanner.py
+++ scanner.py
@@ -8,8 +8,8 @@ class LocalScanner:
         return IP_HOST
     def setSocket(self):
         ip_to_host = self.getHostToscanned()
-        newSocket = socket(AF_INET, SOCK_STREAM)
         for i in range(50, 500):
+            newSocket = socket(AF_INET, SOCK_STREAM)
             connection = newSocket.connect_ex(((ip_to_host, i)))
             if (connection == 0):
                 print('Port %d: Open' % i)
