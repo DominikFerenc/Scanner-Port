f36027b2a879a21aae1edaa1ef080b0c0c6c2ea4
KRebrow <dominikferenc1995@gmail.com>
2020-10-12 13:25:40 +0200

    Update scanner.py
    1.Added entering ip data and port range from console
    2. Bugs fixed

--- scanner.py
+++ scanner.py
@@ -1,25 +1,52 @@
 from socket import *
 
+
 class LocalScanner:
     def getHostToscanned(self):
-        host = input("Enter to host to scanned:")
-        IP_HOST = gethostbyname(host)
-        print("Starting scan on host: ", IP_HOST)
-        return IP_HOST
+        host = input("Enter to host to scanned: ")
+        return gethostbyname(host)
+
+    def getLowestPort(self):
+        return int(input("Enter to lowest port: "))
+
+    def getMaximumPort(self):
+        return int(input("Enter to maximum port: "))
+
+    def startingScan(self, ip_to_host, lowest_port, maximum_port):
+        print("\nStarting scan on host:\033[33m",
+              ip_to_host, "\033[0min the range of:\033[32m", lowest_port, "\033[0mport to:\033[31m", maximum_port, "\033[0mport")
+
+    def showOpenPorts(self, port):
+        print('Port \033[33m%d\033[0m: \033[32m Open\033[0m' % port)
+
     def setSocket(self):
         ip_to_host = self.getHostToscanned()
-        newSocket = socket(AF_INET, SOCK_STREAM)
-        for i in range(50, 500):
-            connection = newSocket.connect_ex(((ip_to_host, i)))
+        lowest_port = self.getLowestPort()
+        maximum_port = self.getMaximumPort()
+        self.startingScan(ip_to_host, lowest_port, maximum_port)
+
+        for port in range(lowest_port, maximum_port):
+            newSocket = socket(AF_INET, SOCK_STREAM)
+            connection = newSocket.connect_ex(((ip_to_host, port)))
             if (connection == 0):
-                print('Port %d: Open' %(i,))
-        newSocket.close()
+                self.showOpenPorts(port)
+            newSocket.close()
+
+
+
+#class Data:
+    #def getAvailablePorts(self):
+    #def presentationData(self):
+
 
 
 
 def main():
     sc = LocalScanner()
-    sc.setSocket()
+    try:
+        sc.setSocket()
+    except (KeyboardInterrupt, SystemExit):
+        print("\n\033[31mThe key combination CTRL+C is pressed.\033[0m")
 
 if __name__ == "__main__":
     main()
